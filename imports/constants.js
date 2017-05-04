5b1d117f567737379b7204c057d78aa2e8cb252b
Yuji Ichikawa <ichikawa.yuji@gmail.com>
2017-05-04 10:50:12 +0900

    production

--- imports/constants.js
+++ imports/constants.js
@@ -1,5 +1,8 @@
 import { DDP } from 'meteor/ddp';
 
 export const KAKO_SELECTOR = { 'twitter.profile.screen_name': 'twiigo2015' };
-export const twiigo = DDP.connect('http://localhost:3000');
+export const twiigo = DDP.connect(process.env.NODE_ENV === 'production' ?
+        'https://twiigo.herokuapp.com'
+    :
+        'http://localhost:3000');
 export const dbOptions = { connection: twiigo };
